af06637e943da66a8f1b75ff9f6760ae4011e204
EC2 Default User <ec2-user@ip-172-31-1-157.ap-southeast-2.compute.internal>
2023-09-14 06:37:17 +0000

    応用２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,6 @@
 class BooksController < ApplicationController
- before_action :authenticate_user!
-  before_action :is_matching_login_user, only: [:edit, :update]
-
+  before_action :authenticate_user!
+ 
 
   def show
     @book = Book.find(params[:id])
